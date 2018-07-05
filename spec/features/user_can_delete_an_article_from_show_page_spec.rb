30b8f40e233a956a5c03bb7f4b053cee8cecd850
JP Lynch <JPLynch35@gmail.com>
2018-07-05 12:26:58 -0600

    Add destroy feature test and destroy method

---
+++ spec/features/user_can_delete_an_article_from_show_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'a visitor' do
+  describe 'visits /articles/:id' do
+    it 'and can delete the viewed article' do
+      article_1 = Article.create!(title: 'Title 1', body: 'Body 1')
+      article_2 = Article.create!(title: 'Title 2', body: 'Body 2')
+
+      visit article_path(article_2)
+      click_link 'Delete'
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content(article_1.title)
+      expect(page).to_not have_content(article_2.title)
+    end
+  end
+end
